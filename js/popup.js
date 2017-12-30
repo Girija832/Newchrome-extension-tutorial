45b8e64c2ce6eadd3968719ce0be46e70cfdedb5
Girija Shankar Sahoo <girija.sahoo@mobtexting.com>
2017-12-30 11:53:56 +0530

    config chrome extension

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+	var checkPageClick = document.getElementById("checkPage");
+	checkPageClick.addEventListener("click", function() {
+		alert("hello");
+
+	});
+});
